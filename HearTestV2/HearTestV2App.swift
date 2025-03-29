fa9d1d0e83cf12ae04f957b782e265eb3df0c303
chrisxladeira <ladeirachris@gmail.com>
2025-03-29 09:55:38 +0200

    Initial Commit

---
+++ HearTestV2/HearTestV2App.swift
@@ -0,0 +1,17 @@
+//
+//  HearTestV2App.swift
+//  HearTestV2
+//
+//  Created by Christopher  Ladeira  on 2025/03/29.
+//
+
+import SwiftUI
+
+@main
+struct HearTestV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
